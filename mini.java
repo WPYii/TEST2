a2df8ba4c6306d0f3ae700ea566b0ebda52a1282
PoYi <wuboyi.wpy@gmail.com>
2022-08-24 23:27:46 +0800

    Edit feature branch

--- mini.java
+++ mini.java
@@ -2,4 +2,8 @@ class minifile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World@!");
+    }
 }
