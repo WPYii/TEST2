03df1394fac3d5799c46e4c940dcd5d436bd30e6
WPYii <88721625+WPYii@users.noreply.github.com>
2022-08-24 23:21:19 +0800

    Update main.java

--- main.java
+++ main.java
@@ -2,4 +2,8 @@ class mainfile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    
+    public static void still_in_main() {
+        System.out.println("Hello, World2!");
+    }
 }
