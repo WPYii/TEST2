5e8a1201aa5a2ba3ecf58b7705808dc3a11b3b06
WPYii <88721625+WPYii@users.noreply.github.com>
2022-08-24 23:22:46 +0800

    Update mian.java

--- mian.java
+++ mian.java
@@ -2,4 +2,8 @@ class mianfile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    
+    public static void still_in_main() {
+        System.out.println("efa");
+    }
 }
