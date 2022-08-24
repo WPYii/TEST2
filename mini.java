add1ce85ae5272ef88bf0762374e9386d77d32af
PoYi <wuboyi.wpy@gmail.com>
2022-08-24 23:48:24 +0800

    dev

---
+++ mini.java
@@ -0,0 +1,13 @@
+class minifile {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World@!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello dev");
+    }
+}
